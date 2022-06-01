350f2f781d66dbf835e991c1c77c58c074da7c30
Trinité <zinsoutrinite@gmail.com>
2022-06-01 09:34:00 +0100

    improvement

--- multitenant/settings.py
+++ multitenant/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = 'django-insecure-t*93$=ah(+#+^2lrthe&k6qh7jg@0d$kf!!o4ldr#!0a(=n#-z
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.multi.mydev',]
 
 SHARED_APPS = (
     'django_tenants',  # mandatory, should always be before any django app
@@ -162,9 +162,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APPEND_SLASH = True
